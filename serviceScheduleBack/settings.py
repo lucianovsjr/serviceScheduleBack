9990a49aa75f325bc66f921bd42dfd8a89466325
lucianovsjr <lucianovsjr@gmail.com>
2020-10-12 14:02:42 -0300

    Debug Propagate

--- serviceScheduleBack/settings.py
+++ serviceScheduleBack/settings.py
@@ -19,6 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
